8e6b9c0dd9e67854446f94e506531cdaf3fef11f
M Yusril Ihza Mahendra <yusrilza201@gmail.com>
2024-05-19 15:08:13 +0000

    feat: add arguments

--- element.sh
+++ element.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+PSQL=PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [ -z "$1" ]
 then
   echo "Please provide an element as an argument."
   exit 1
-
 fi
+
+ARG=$1
+
+
