b186ca3e5154de79a6343483d357dc3a35425ac3
M Yusril Ihza Mahendra <yusrilza201@gmail.com>
2024-05-19 15:42:23 +0000

    feat: build element info

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL=PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [ $# -eq 0 ]
 then
@@ -10,5 +10,20 @@ fi
 
 ARG=$1
 
+ELEMENT_INFO() {
+  local QUERY=$1
+  ELEMENT_RESULT=$($PSQL "$QUERY")
+
+  if [[ -z $ELEMENT_RESULT ]]; then
+    echo "I could not find that element in the database."
+    exit 0
+  fi
+echo $ELEMENT_RESULT | while IFS=" | " read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT; do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
+}
+
+# Get and display element info
+ELEMENT_INFO "$QUERY"
 
 
